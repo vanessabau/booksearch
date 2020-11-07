3142d5cf8a63be99c9576f7871db6c6fdd184b27
Vanessa Bautista <vbautista916@icloud.com>
2020-11-07 10:42:33 -0800

    updating with atlas

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const db = require("../models");
 
 // This file empties the Books collection and inserts the books below
-
+//MONGODB_URI
 mongoose.connect(
-  process.env.MONGODB_URI ||
+  process.env.DB_URI ||
   "mongodb://localhost/reactreadinglist"
 );
 
